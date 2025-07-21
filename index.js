642805f0dc2068306f0d6440c5f294174fae1cb1
Rushikesh <rushikeshexperimental@gmail.com>
2025-07-21 10:29:30 +0530

    added health check API

--- index.js
+++ index.js
@@ -120,6 +120,12 @@ app.delete("/contacts/:id", (req, res) => {
   });
 });
 
+app.get("/health-check", (req, res) => {
+    return res.status(200).json({
+        message: "API is up and running."
+    });
+})
+
 // Set up server to listen on a port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
